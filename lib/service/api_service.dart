b4a9dd4b4973c788aac697d5d3a565e503e440c3
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-18 23:42:18 -0300

    função para pegar clima semanal

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -3,34 +3,60 @@ import 'package:http/http.dart' as http;
 import 'package:weather_station/model/clima_model.dart';
 
 class ApiService {
-    static ApiService? _apiService;
+  static ApiService? _apiService;
 
-   ApiService get getInstance {
+  ApiService get getInstance {
     _apiService ??= ApiService();
     return _apiService!;
   }
 
- Future<List<ClimaModel>> pegarClimaSemanal() async {
-  String linkApi = "https://api.hgbrasil.com/weather?format=json-cors&key=ea69fe0d";
-  
-  try {
-    var resposta = await http.get(Uri.parse(linkApi));
+  Future<List<ClimaModel>> pegarClimaSemanal() async {
+    String linkApi =
+        "https://api.hgbrasil.com/weather?format=json-cors&key=ea69fe0d";
+    List<ClimaModel> previsao = [];
 
-    if (resposta.statusCode == 200) {
-      var dados = json.decode(resposta.body);
+    try {
+      var resposta = await http.get(Uri.parse(linkApi));
 
-      // Exemplo: pegando temperatura e cidade
-      var cidade = dados["results"]["city"];
-      var temp = dados["results"]["temp"];
+      if (resposta.statusCode == 200) {
+        var dados = json.decode(resposta.body);
+        var cidade = dados["results"]["city"];
+        var temperaturaAtual = dados["results"]["temp"];
 
-      print("Cidade: $cidade");
-      print("Temperatura: $temp°C");
-    } else {
-      print("Erro na requisição: ${resposta.statusCode}");
+        var lista = dados["results"]["forecast"];
+        for (var dia in lista) {
+          ClimaModel clima = ClimaModel(
+              nome: cidade,
+              temperatura: temperaturaAtual,
+              data: dia["date"] ?? '',
+              diaSemana: dia["weekday"] ?? '',
+              descriptionClima: dia["description"] ?? '',
+              tempMin: dia["min"] ?? 0,
+              tempMax: dia["max"] ?? 0,
+              icone: dia["condition"] ?? '');
+
+          if (previsao.length == 7) break;
+
+
+          previsao.add(clima);
+        }
+        print( previsao[0].tempMax);
+        
+        print( previsao[1].tempMax);
+        print( previsao[2].tempMax);
+        print( previsao[3].tempMax);
+        print( previsao[4].tempMax);
+        print( previsao[5].tempMax);
+        
+        print( previsao[6].tempMax);
+        return previsao;
+      } else {
+        print("Erro na requisição: ${resposta.statusCode}");
+        return [];
+      }
+    } catch (e) {
+      print("Erro: $e");
+      return [];
     }
-  } catch (e) {
-    print("Erro: $e");
   }
- }
-  
 }
