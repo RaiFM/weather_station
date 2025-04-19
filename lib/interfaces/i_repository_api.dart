20a84507af6ab36a1c207b2d0f25f3eec7342f42
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-19 11:40:52 -0300

    atualizando retorno e adicionando outro metodo ao i repository

--- lib/interfaces/i_repository_api.dart
+++ lib/interfaces/i_repository_api.dart
@@ -1,6 +1,7 @@
 import 'package:weather_station/model/clima_model.dart';
 
 abstract class IRepositoryApi {
-  Future<List<ClimaModel>> listarClima();
-  Future<List<ClimaModel>?> listarClimaSalvos(List<String?> nomeCidades);
+  Future<List<ClimaModel?>> listarClimaNome(String nomeCidade);
+  Future<List<ClimaModel?>> listarClimaLatLong(String latLon);
+  Future<List<ClimaModel?>> listarClimaSalvos(List<String?> nomeCidades);
 }
