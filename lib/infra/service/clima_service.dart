6943da846c560c610a752c10b18425d6fb7342b5
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-19 23:22:11 -0300

    fazendo requisação pro banco

--- lib/infra/service/clima_service.dart
+++ lib/infra/service/clima_service.dart
@@ -1,3 +1,28 @@
+import 'package:weather_station/domain/model/clima_model.dart';
+import 'package:weather_station/external/firestore_db.dart';
+
 class ClimaService {
-  
+      static ClimaService? _climaService;
+      ClimaService get getInstance{
+        _climaService ??= ClimaService();
+        return _climaService!;
+      }        
+
+    final db = FirestoreDB.getInstance;
+
+                                                                                                                                                    void salvarCidade(String nomeCidade) async {
+  final cidadeMap = <String, String>{
+    "nomeCidade": nomeCidade,
+  };
+
+ try {
+    print("Cidade salva com sucesso");
+    await db.collection("climaAPP").add(cidadeMap); 
+  } catch (e) {
+    print("Erro ao adicionar cidade: $e");
+  }
+
+
+}
+                                                                      
 }
