01b81396fdca7706b03a9f6d96f8e2d43ad42aca
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-22 20:40:52 -0300

    exlcuir cidade uc

--- lib/domain/usecase/excluir_cidade_uc.dart
+++ lib/domain/usecase/excluir_cidade_uc.dart
@@ -1,3 +1,22 @@
+import 'package:weather_station/domain/model/clima_model.dart';
+import 'package:weather_station/infra/repository/repository_clima_firebase.dart';
+
 class ExcluirCidadeUc {
+  static ExcluirCidadeUc? _excluirCidadeUc;
+
+  ExcluirCidadeUc get getInstance{
+    _excluirCidadeUc ??= ExcluirCidadeUc(repositoryClimaFirebase: repositoryClimaFirebase.getInstance);
+    return _excluirCidadeUc!;
+  }
+
+  ExcluirCidadeUc({required this.repositoryClimaFirebase});
+  final RepositoryClimaFirebase repositoryClimaFirebase;
+
   
+  void excluir(ClimaModel climaModel) {
+    if(climaModel.nome.isEmpty){
+      throw Exception("Não foi possível excluir a cidade pois ela não existe");
+    }
+    repositoryClimaFirebase.excluirCidade(climaModel);
+  }
 }
