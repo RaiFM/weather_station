27f6375f2901817925116df4f9d12cedd079c046
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-22 10:40:39 -0300

    tirando dependencia do repository api de pegar os nomes

--- lib/infra/interfaces/i_repository_api.dart
+++ lib/infra/interfaces/i_repository_api.dart
@@ -3,5 +3,5 @@ import 'package:weather_station/domain/model/clima_model.dart';
 abstract class IRepositoryApi {
   Future<List<ClimaModel?>> listarClimaNome(String nomeCidade);
   Future<List<ClimaModel?>> listarClimaLatLong(String latLon);
-  Future<List<ClimaModel?>> listarClimaSalvos(List<String?> nomeCidades);
+  Future<List<ClimaModel?>> listarClimaSalvos();
 }
