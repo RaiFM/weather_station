11af99355cbda00527090e578fb690cad736fe03
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-22 13:56:13 -0300

    implementando o repository

--- lib/infra/repository/repository_clima_firebase.dart
+++ lib/infra/repository/repository_clima_firebase.dart
@@ -1,23 +1,52 @@
+import 'package:weather_station/infra/interfaces/i_repository_api.dart';
 import 'package:weather_station/infra/interfaces/i_repository_clima.dart';
 import 'package:weather_station/domain/model/clima_model.dart';
 import 'package:weather_station/infra/service/clima_service.dart';
 
 class RepositoryClimaFirebase implements IRepositoryClima {
+  static RepositoryClimaFirebase? _repositoryClimaFirebase;
+
+  RepositoryClimaFirebase get getInstance {
+    _repositoryClimaFirebase ??= RepositoryClimaFirebase(
+        climaService: climaService.getInstance);
+    return _repositoryClimaFirebase!;
+  }
+
   final ClimaService climaService;
+  
 
-  RepositoryClimaFirebase({
-    required this.climaService
-  });
+  RepositoryClimaFirebase(
+      {required this.climaService});
 
   @override
-  List<ClimaModel?> listarLugaresSalvos() {
-   
-    throw UnimplementedError();
+  Future<List<ClimaModel?>> listarLugaresSalvos() async {
+    try {
+     
+       List<ClimaModel?> listaLugares = await climaService.listarCidades();
+      return listaLugares;
+    } catch (e) {
+      Exception("Erro ao listar cidades salvas");
+      return [];
+    }
   }
 
   @override
-  void salvarLocalizacao(ClimaModel climaModel) {
+  void salvarCidade(ClimaModel climaModel) {
+    try {
+      String nome = climaModel.nome;
+      climaService.salvarCidade(nome);
+    } catch (e) {
+      Exception("Erro ao excluir cidade");
+    }
   }
 
-
+  @override
+  void excluirCidade(ClimaModel climaModel) {
+    try {
+      String nome = climaModel.nome;
+      climaService.excluirCidade(nome);
+    } catch (e) {
+      Exception("Erro ao excluir cidade");
+    }
+  }
 }
