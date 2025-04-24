4934d376ce445580fedcbe00511fcc2b825586c7
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-04-24 19:06:13 -0300

    uc salvar cidade

--- lib/domain/usecase/salvar_cidade_uc.dart
+++ lib/domain/usecase/salvar_cidade_uc.dart
@@ -1,3 +1,33 @@
+import 'package:weather_station/domain/model/clima_model.dart';
+import 'package:weather_station/infra/interfaces/i_repository_clima.dart';
+import 'package:weather_station/infra/repository/repository_api.dart';
+import 'package:weather_station/infra/repository/repository_clima_firebase.dart';
+import 'package:weather_station/infra/service/api_service.dart';
+import 'package:weather_station/infra/service/clima_service.dart';
+
 class SalvarCidadeUc {
-  
+  static SalvarCidadeUc? _salvarCidadeUc;
+
+  SalvarCidadeUc get getInstance {
+    _salvarCidadeUc ??= SalvarCidadeUc(
+      repositoryClima: RepositoryClimaFirebase(
+        climaService: ClimaService(
+          iRepositoryApi: RepositoryApi(
+            apiService: ApiService().getInstance,
+          ).getInstance,
+        ).getInstance,
+      ).getInstance,
+    );
+    return _salvarCidadeUc!;
+  }
+
+  SalvarCidadeUc({required this.repositoryClima});
+  final IRepositoryClima repositoryClima;
+
+  void salvar(ClimaModel climaModel) {
+    if (climaModel.nome.isEmpty) {
+      throw Exception("Não é possível salvar uma cidade sem nome dela.");
+    }
+    repositoryClima.salvarCidade(climaModel);
+  }
 }
