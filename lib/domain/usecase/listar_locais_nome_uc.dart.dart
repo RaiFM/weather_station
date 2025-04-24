16b89904049543bac9edfb07fc3159e69543d9ad
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-04-24 19:06:32 -0300

    uc listar local nome

--- lib/domain/usecase/listar_locais_nome_uc.dart.dart
+++ lib/domain/usecase/listar_locais_nome_uc.dart.dart
@@ -1,3 +1,27 @@
-class ListarLocaisNomeUc {
-  
+import 'package:weather_station/domain/model/clima_model.dart';
+import 'package:weather_station/infra/interfaces/i_repository_api.dart';
+import 'package:weather_station/infra/repository/repository_api.dart';
+import 'package:weather_station/infra/service/api_service.dart';
+
+class ListarLocaisPorNomeUc {
+  static ListarLocaisPorNomeUc? _listarLocaisPorNomeUc;
+
+  ListarLocaisPorNomeUc get getInstance {
+    _listarLocaisPorNomeUc ??= ListarLocaisPorNomeUc(
+      iRepositoryApi: RepositoryApi(
+        apiService: ApiService().getInstance,
+      ).getInstance,
+    );
+    return _listarLocaisPorNomeUc!;
+  }
+
+  ListarLocaisPorNomeUc({required this.iRepositoryApi});
+  final IRepositoryApi iRepositoryApi;
+
+  Future<List<ClimaModel?>> buscarPorNome(String nome) async {
+    if (nome.isEmpty) {
+      throw Exception("Nome da cidade não pode estar vazio.");
+    }
+    return await iRepositoryApi.listarClimaNome(nome);
+  }
 }
