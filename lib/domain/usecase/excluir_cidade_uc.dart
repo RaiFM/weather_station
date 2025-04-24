255a16f7bf4be1045d12dffe14b009e7623c551b
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-04-23 22:10:10 -0300

    repassando caminho excluir cidade

--- lib/domain/usecase/excluir_cidade_uc.dart
+++ lib/domain/usecase/excluir_cidade_uc.dart
@@ -1,22 +1,26 @@
 import 'package:weather_station/domain/model/clima_model.dart';
+import 'package:weather_station/infra/interfaces/i_repository_clima.dart';
+import 'package:weather_station/infra/repository/repository_api.dart';
 import 'package:weather_station/infra/repository/repository_clima_firebase.dart';
+import 'package:weather_station/infra/service/api_service.dart';
+import 'package:weather_station/infra/service/clima_service.dart';
 
 class ExcluirCidadeUc {
   static ExcluirCidadeUc? _excluirCidadeUc;
 
   ExcluirCidadeUc get getInstance{
-    _excluirCidadeUc ??= ExcluirCidadeUc(repositoryClimaFirebase: repositoryClimaFirebase.getInstance);
+    _excluirCidadeUc ??= ExcluirCidadeUc(repositoryClima: RepositoryClimaFirebase(climaService: ClimaService(iRepositoryApi: RepositoryApi(apiService: ApiService().getInstance).getInstance).getInstance).getInstance);
     return _excluirCidadeUc!;
   }
 
-  ExcluirCidadeUc({required this.repositoryClimaFirebase});
-  final RepositoryClimaFirebase repositoryClimaFirebase;
+  ExcluirCidadeUc({required this.repositoryClima});
+  final IRepositoryClima repositoryClima;
 
   
   void excluir(ClimaModel climaModel) {
     if(climaModel.nome.isEmpty){
       throw Exception("Não foi possível excluir a cidade pois ela não existe");
     }
-    repositoryClimaFirebase.excluirCidade(climaModel);
+    repositoryClima.excluirCidade(climaModel);
   }
 }
