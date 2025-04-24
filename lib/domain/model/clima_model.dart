d517c57dba30243717359763cc6afad81b3ac2ad
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-04-24 11:43:05 -0300

    adicionando ultimo dado

--- lib/domain/model/clima_model.dart
+++ lib/domain/model/clima_model.dart
@@ -1,4 +1,3 @@
-import 'dart:ffi';
 
 class ClimaModel {
   String nome;
@@ -10,9 +9,10 @@ class ClimaModel {
   int tempMax;
   String icone;
   int umidade;
-  int precipitacao;
+  int chanceChuva;
+  double precipitacao;
   String velocidadeVento;
-  String direcaoVento;
+  int direcaoVento;
   String cardinalVento;
   String nascerSol;
   String  porSol;
@@ -30,6 +30,7 @@ class ClimaModel {
     required this.tempMax,
     required this.icone,
     required this.precipitacao,
+    required this.chanceChuva,
     required this.umidade,
     required this.velocidadeVento, 
     required this.direcaoVento, 
@@ -40,28 +41,4 @@ class ClimaModel {
     required this.fusoHorario,
   } 
   );
-
-  factory ClimaModel.fromJson(Map<String, dynamic> json) {
-    // Verificando se os dados do JSON têm o formato correto
-    return ClimaModel(
-      nome: json['city_name'] ?? 'Cidade não disponível',  
-      data: json['date'] ?? 'Data não disponível',        
-      temperatura: json['temp'] ?? 0,
-      descriptionClima: json['description'] ?? 'Sem descrição',  
-      diaSemana: json['weekday'] ?? 'Dia não disponível',  
-      tempMax: json['max'] ?? 0,  
-      tempMin: json['min'] ?? 0,  
-      icone: json['img_id'] ?? '',
-      precipitacao: json['rain'] ?? 0, 
-      umidade:  json['humidity'],
-      velocidadeVento: json['wind_speedy'] ?? 0,
-      direcaoVento: json['wind_direction'] ?? 0,
-      cardinalVento: json['wind_cardinal'] ?? '',
-      nascerSol: json['sunrise'] ?? '',
-      porSol: json['sunset'] ?? '',
-      faseLua: json['moon_phase'] ?? 'Sem descrição',
-      fusoHorario: json['timezone'] ?? 0,
-
-    );
-  } 
   }
