5ba3c578c465761a17633eb7d6154a8dab6e262a
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-04-24 19:06:22 -0300

    uc listar local lat lon

--- lib/domain/usecase/listar_locais_lat_lon_uc.dart
+++ lib/domain/usecase/listar_locais_lat_lon_uc.dart
@@ -1,3 +1,28 @@
-class ListarLocaisLatLonUc {
-  
+import 'package:weather_station/domain/model/clima_model.dart';
+import 'package:weather_station/infra/interfaces/i_repository_api.dart';
+import 'package:weather_station/infra/repository/repository_api.dart';
+import 'package:weather_station/infra/service/api_service.dart';
+
+class ListarLocaisPorLatLonUc {
+  static ListarLocaisPorLatLonUc? _listarLocaisPorLatLonUc;
+
+  ListarLocaisPorLatLonUc get getInstance {
+    _listarLocaisPorLatLonUc ??= ListarLocaisPorLatLonUc(
+      iRepositoryApi: RepositoryApi(
+        apiService: ApiService().getInstance,
+      ).getInstance,
+    );
+    return _listarLocaisPorLatLonUc!;
+  }
+
+  ListarLocaisPorLatLonUc({required this.iRepositoryApi});
+  final IRepositoryApi iRepositoryApi;
+
+  Future<List<ClimaModel?>> buscarPorLatLon(int lat, int lon) async {
+    if (lat == 0.0 || lon == 0.0) {
+      throw Exception("Latitude ou longitude inválidas.");
+    }
+    String latLon = lat.toString() + lon.toString();
+    return await iRepositoryApi.listarClimaLatLong(latLon);
+  }
 }
