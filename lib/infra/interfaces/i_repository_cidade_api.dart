89c0b861be594bdca25755b8277c91b59c895610
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-04-24 19:05:58 -0300

    repository cidade api

---
+++ lib/infra/interfaces/i_repository_cidade_api.dart
@@ -0,0 +1,3 @@
+abstract class IRepositoryCidadeApi {
+  Future<List<String>> listarCidades(int uf);
+}
---
+++ lib/infra/repository/repository_cidade_api.dart
@@ -0,0 +1,25 @@
+import 'package:weather_station/infra/interfaces/i_repository_cidade_api.dart';
+import 'package:weather_station/infra/service/api_cidades_service.dart';
+
+class RepositoryCidadeApi implements IRepositoryCidadeApi{
+  static RepositoryCidadeApi? _repositoryCidadeApi;
+  
+  RepositoryCidadeApi get getInstance{
+    _repositoryCidadeApi ??= RepositoryCidadeApi(apiCidadesService: apiCidadesService.getInstance);
+    return _repositoryCidadeApi!;
+  }
+  RepositoryCidadeApi({
+    required this.apiCidadesService,
+  });
+
+  ApiCidadesService apiCidadesService;
+
+
+
+  @override
+  Future<List<String>> listarCidades(int ufUser) async {
+    List<String> lista = await apiCidadesService.getMunicipios(ufUser);
+    return lista;
+  }
+  
+}
