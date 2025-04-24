61ab83e6af5887829bae5ac99c945aa7dda6c797
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-04-24 19:06:45 -0300

    uc listar local salvos

--- lib/domain/usecase/listar_locais_salvos_uc.dart
+++ lib/domain/usecase/listar_locais_salvos_uc.dart
@@ -1,3 +1,27 @@
+import 'package:weather_station/domain/model/clima_model.dart';
+import 'package:weather_station/infra/interfaces/i_repository_api.dart';
+import 'package:weather_station/infra/repository/repository_api.dart';
+import 'package:weather_station/infra/service/api_service.dart';
+
 class ListarLocaisSalvosUc {
-  
+  static ListarLocaisSalvosUc? _listarLocaisSalvosUc;
+
+  ListarLocaisSalvosUc get getInstance {
+    _listarLocaisSalvosUc ??= ListarLocaisSalvosUc(
+      iRepositoryApi: RepositoryApi(
+        apiService: ApiService().getInstance,
+      ).getInstance,
+    );
+    return _listarLocaisSalvosUc!;
+  }
+
+  ListarLocaisSalvosUc({required this.iRepositoryApi});
+  final IRepositoryApi iRepositoryApi;
+
+  Future<List<ClimaModel?>> buscar(List<String?> nomeCidade) async {
+    if (nomeCidade.isEmpty) {
+      throw Exception("Não há cidades.");
+    }
+    return await iRepositoryApi.listarClimaSalvos(nomeCidade);
+  }
 }
