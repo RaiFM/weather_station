9dddedd1105a253574cf463b0181b3a86d378150
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-04-24 21:03:57 -0300

    arrumando confusao anterior

--- lib/domain/usecase/listar_locais_salvos_uc.dart
+++ lib/domain/usecase/listar_locais_salvos_uc.dart
@@ -1,27 +1,25 @@
 import 'package:weather_station/domain/model/clima_model.dart';
 import 'package:weather_station/infra/interfaces/i_repository_api.dart';
+import 'package:weather_station/infra/interfaces/i_repository_clima.dart';
 import 'package:weather_station/infra/repository/repository_api.dart';
+import 'package:weather_station/infra/repository/repository_clima_firebase.dart';
 import 'package:weather_station/infra/service/api_service.dart';
+import 'package:weather_station/infra/service/clima_service.dart';
 
 class ListarLocaisSalvosUc {
   static ListarLocaisSalvosUc? _listarLocaisSalvosUc;
 
   ListarLocaisSalvosUc get getInstance {
     _listarLocaisSalvosUc ??= ListarLocaisSalvosUc(
-      iRepositoryApi: RepositoryApi(
-        apiService: ApiService().getInstance,
-      ).getInstance,
+      iRepositoryClima: RepositoryClimaFirebase(climaService: ClimaService(iRepositoryApi: RepositoryApi(apiService: ApiService().getInstance).getInstance).getInstance).getInstance,
     );
     return _listarLocaisSalvosUc!;
   }
 
-  ListarLocaisSalvosUc({required this.iRepositoryApi});
-  final IRepositoryApi iRepositoryApi;
+  ListarLocaisSalvosUc({required this.iRepositoryClima});
+  final IRepositoryClima iRepositoryClima;
 
-  Future<List<ClimaModel?>> buscar(List<String?> nomeCidade) async {
-    if (nomeCidade.isEmpty) {
-      throw Exception("Não há cidades.");
-    }
-    return await iRepositoryApi.listarClimaSalvos(nomeCidade);
+  Future<List<ClimaModel?>> buscar() async {
+    return await iRepositoryClima.listarLugaresSalvos();
   }
 }
