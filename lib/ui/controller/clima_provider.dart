00b21997f51dc3440789119ca50241ac45d3e4ff
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-04-24 21:04:11 -0300

    criando clima provider

---
+++ lib/ui/controller/clima_provider.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/foundation.dart';
+import 'package:weather_station/domain/model/clima_model.dart';
+import 'package:weather_station/domain/usecase/listar_locais_lat_lon_uc.dart';
+import 'package:weather_station/domain/usecase/listar_locais_nome_uc.dart.dart';
+import 'package:weather_station/domain/usecase/listar_locais_salvos_uc.dart';
+
+class ClimaProvider extends ChangeNotifier {
+
+
+   void notify() {
+    notifyListeners();
+  }
+  ListarLocaisPorLatLonUc listarLocaisPorLatLonUc;
+  ListarLocaisPorNomeUc listarLocaisPorNomeUc;
+  ListarLocaisSalvosUc listarLocaisSalvosUc;
+
+  
+  ClimaProvider({required this.listarLocaisPorLatLonUc, required this.listarLocaisPorNomeUc, required this.listarLocaisSalvosUc});
+
+
+
+ List<ClimaModel?> _allClimaPrincipal = [];
+ List<ClimaModel?> _allClimaSalvos = [];
+ 
+
+  List<ClimaModel?> get allClimas => _allClimaPrincipal;
+  List<ClimaModel?> get allClimasSalvos => _allClimaSalvos;
+
+
+  Future<List<ClimaModel?>> getClimaLatLon(double lat, double lon) async {
+    _allClimaPrincipal = [];
+   return _allClimaPrincipal = await listarLocaisPorLatLonUc.getInstance.buscarPorLatLon(lat, lon);
+  }
+
+  Future<List<ClimaModel?>> getClimaNome(String nome) async {
+    _allClimaPrincipal = [];
+   return _allClimaPrincipal = await listarLocaisPorNomeUc.getInstance.buscarPorNome(nome);
+  }
+  
+  Future<List<ClimaModel?>> getClimaSalvos() async {
+    _allClimaSalvos = [];
+   return _allClimaSalvos = await listarLocaisSalvosUc.getInstance.buscar();
+   
+  }
+
+}
