5576f14ace18834f6cf418b91b46baddec4a0d4d
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2025-04-30 12:50:40 -0300

    arrumando nome da classe + logica de tentativas de acessar as settings

--- lib/ui/controller/location_controller.dart
+++ lib/ui/controller/location_controller.dart
@@ -1,18 +1,34 @@
 import 'package:geolocator/geolocator.dart';
 
-class ClimaController {
-
+class LocationController {
   Future<Position> posicaoAtual() async {
     bool localizacaoAtiva;
     LocationPermission locationPermission;
+    int retryCount = 0;
+    const int maxRetries = 3;
 
     localizacaoAtiva = await Geolocator.isLocationServiceEnabled();
-    if (!localizacaoAtiva) {
-      return Future.error('Serviço de localização desativado');
+
+    while (!localizacaoAtiva) {
+      // Solicitar ao usuário que ative o serviço de localização
+      await Geolocator.openLocationSettings();
+      localizacaoAtiva = await Geolocator.isLocationServiceEnabled();
+      if (!localizacaoAtiva && retryCount < maxRetries) {
+        retryCount++;
+        await Future.delayed(const Duration(seconds: 5));
+        continue;
+      } else {
+        break;
+      }
     }
+
+    if (!localizacaoAtiva) {
+      return Future.error("Serviço de localização indisponivel");
+    }
+
     locationPermission = await Geolocator.checkPermission();
     if (locationPermission == LocationPermission.denied) {
-        locationPermission = await Geolocator.requestPermission();
+      locationPermission = await Geolocator.requestPermission();
       if (locationPermission == LocationPermission.denied) {
         return Future.error("Serviço de localização indisponivel");
       }
@@ -23,8 +39,7 @@ class ClimaController {
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
+
     return await Geolocator.getCurrentPosition();
   }
-
-
 }
