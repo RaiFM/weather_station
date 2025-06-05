db9b5b29afb35172779fa915118e75b0ad75f520
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-06-05 20:46:34 -0300

    slavar cidade uc

--- lib/domain/usecase/salvar_cidade_uc.dart
+++ lib/domain/usecase/salvar_cidade_uc.dart
@@ -2,6 +2,7 @@ import 'package:weather_station/domain/model/clima_model.dart';
 import 'package:weather_station/infra/interfaces/i_repository_clima.dart';
 import 'package:weather_station/infra/repository/repository_clima_firebase.dart';
 
+
 class SalvarCidadeUc {
   static SalvarCidadeUc? _salvarCidadeUc;
 
@@ -12,13 +13,15 @@ class SalvarCidadeUc {
     return _salvarCidadeUc!;
   }
 
-  SalvarCidadeUc({required this.repositoryClima});
+    SalvarCidadeUc({required this.repositoryClima});
   final IRepositoryClima repositoryClima;
 
-  void salvar(ClimaModel climaModel) {
+  void salvar(ClimaModel climaModel) async {
     if (climaModel.nome.isEmpty) {
       throw Exception("Não é possível salvar uma cidade sem nome dela.");
     }
+    final listarLugaresSalvos = await repositoryClima.listarLugaresSalvos();
+    if(listarLugaresSalvos.length > 10) throw Exception("Não é possivel salvar mais que dez cidades");
     repositoryClima.salvarCidade(climaModel);
   }
 }
